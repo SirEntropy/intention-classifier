dbf6f48c9dd475a70841fbf69530d1fa89cd16b2
Rick Chen <rickchen19970829@outlook.com>
2024-02-01 01:16:16 -0800

    roll back

--- classifier.py
+++ classifier.py
@@ -1,24 +1,2 @@
-import openai
-from openai import OpenAI
-import os
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
-def classify_text(text):
-    base_prompt = """
-    Please classify the user questions into one of the following categories:
-    1. Greetings
-    2. Data Security
-    3. Software Development
-    4. Documentation
-    """
-    messages = [{"role": "system", "content": base_prompt}, {"role": "user", "content": text}]
-    
-    client = OpenAI()
-    res = client.chat.completions.create(
-        model = "gpt-4-1106-preview",
-        messages = messages
-    )
-    
-    return res
     
